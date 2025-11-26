a46d00045e4745065384058a304ee3bcc71b6bc4
dongduy12 <dongduy261299@gmail.com>
2025-11-26 23:09:25 +0700

    Fix locale routing

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  i18n: {
-    locales: ["vi", "en", "ja"],
-    defaultLocale: "vi",
-  },
 };
 
 export default nextConfig;
